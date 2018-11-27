0264a7e26706d2c6c577fa2974ddb67666c96d0e
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-11-27 23:12:29 +0300

    Create Class to Store Call Information

---
+++ TelephoneServiceProvider.BillingSystem/Call.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TelephoneServiceProvider.BillingSystem
+{
+    public class Call
+    {
+        public string SenderPhoneNumber { get; set; }
+
+        public string ReceiverPhoneNumber { get; set; }
+
+        public DateTime CallStartTime { get; set; }
+
+        public DateTime CallEndTime { get; set; }
+
+        public TimeSpan Duration => CallEndTime - CallStartTime;
+
+        public Call(string senderPhoneNumber, string receiverPhoneNumber)
+        {
+            SenderPhoneNumber = senderPhoneNumber;
+
+            ReceiverPhoneNumber = receiverPhoneNumber;
+        }
+    }
+}
