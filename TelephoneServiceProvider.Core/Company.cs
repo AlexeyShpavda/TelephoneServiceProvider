2635f9436d070650ce3f1b9b8461acfecc3df1cb
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-11-28 01:08:15 +0300

    Create Class Communication Company

---
+++ TelephoneServiceProvider.Core/Company.cs
@@ -0,0 +1,12 @@
+﻿namespace TelephoneServiceProvider.Core
+{
+    public class Company
+    {
+        public string Name { get; private set; }
+
+        public Company(string name)
+        {
+            Name = name;
+        }
+    }
+}
