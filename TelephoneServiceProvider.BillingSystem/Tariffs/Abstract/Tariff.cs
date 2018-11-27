35c63208af7da071a709304bed666e604a0661a5
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-11-28 01:38:30 +0300

    Create Abstract Tariff Class and Specific Homebody Class

---
+++ TelephoneServiceProvider.BillingSystem/Tariffs/Abstract/Tariff.cs
@@ -0,0 +1,7 @@
+﻿namespace TelephoneServiceProvider.BillingSystem.Tariffs.Abstract
+{
+    public abstract class Tariff
+    {
+        public abstract decimal CostPerMonth { get; }
+    }
+}
---
+++ TelephoneServiceProvider.BillingSystem/Tariffs/Homebody.cs
@@ -0,0 +1,13 @@
+﻿using TelephoneServiceProvider.BillingSystem.Tariffs.Abstract;
+
+namespace TelephoneServiceProvider.BillingSystem.Tariffs
+{
+    public class Homebody : Tariff
+    {
+        public override decimal CostPerMonth { get; } = 15m;
+
+        public int FreeMinutes { get; } = 1000;
+
+        public double Megabytes { get; } = 3000;
+    }
+}
